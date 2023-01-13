112e788e01b52857fd41a09defa42c5adbfa8309
homeworkdome <homeworkdome@outlook.com>
2023-01-13 12:52:12 +0300

    Добавьте файлы проекта.

---
+++ Задача 10/Navigator.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_10
+{
+    internal class Navigator
+    {
+        int gradus;
+        int min;
+        int sec;
+
+        public int Gradus
+        {
+            get
+            {
+                return gradus;
+            }
+            set
+            {
+               gradus = value % 360;
+            }
+        }
+
+        public int Min
+        {
+            get
+            {
+                return min;
+            }
+            set
+            {
+                Gradus += value / 360;
+                min = value % 60;
+            }
+        }
+
+        public int Sec
+        {
+            get
+            {
+                return sec;
+            }
+            set
+            {
+                Min += value / 60;
+                sec = value % 60;
+            }
+        }
+
+        public Navigator(int gradus, int min, int sec)
+        {
+            this.Gradus = gradus;
+            this.Min = min;
+            this.Sec = sec;
+        }
+        public double ToRadians()
+        {
+            return sec + min * ((Math.PI) / 648000) + gradus * (((Math.PI) / 648000) * ((Math.PI) / 10800));
+        }
+    }
+}
---
+++ Задача 10/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Xml.XPath;
+using Задача_10;
+int gradus_ = Convert.ToInt32(Console.ReadLine());
+int min_ = Convert.ToInt32(Console.ReadLine());
+int sec_ = Convert.ToInt32(Console.ReadLine());
+
+Navigator navigator = new Navigator (gradus_, min_, sec_);
+
+//Navigator navigator = new Navigator() { gradus= gradus_, min=min_, sec=sec_};
+//navigator.gradus = Convert.ToInt32(Console.ReadLine());
+//navigator.min = Convert.ToInt32(Console.ReadLine());
+//navigator.sec = Convert.ToInt32(Console.ReadLine());
+double totalradians = navigator.ToRadians();
+Console.Write(totalradians);
+
